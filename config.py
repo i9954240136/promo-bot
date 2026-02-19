0818856d2f289cfd14e37a3123b0782688698af5
i9954240136 <i9954240136@gmail.com>
2026-02-19 15:14:28 +0300

    Update config.py

--- config.py
+++ config.py
@@ -14,4 +14,12 @@ WEBAPP_URL = os.getenv("WEBAPP_URL", "https://google.com")
 ADMIN_ID = 123456789  
 
 # База данных
+
 DATABASE_NAME = "promo.db"
+
+# Для Render
+import os
+WEBAPP_URL = os.getenv("WEBAPP_URL", "https://google.com")
+ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+DATABASE_URL = os.getenv("DATABASE_URL", "")
