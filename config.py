bc441b9dfbcc0745858af08356ba723d6ffe88c6
i9954240136 <i9954240136@gmail.com>
2026-02-19 15:22:01 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,23 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# 🔐 ВСТАВЬТЕ СЮДА ВАШ НОВЫЙ ТОКЕН (в кавычках)
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8544537463:AAFs2rNEHBNoYiJS45yulmhSXkMF0DYjewI")
-
-# Ссылка на веб-приложение (замените после Шага 3)
-# Пока оставьте заглушку, мы вернемся к этому позже
-WEBAPP_URL = os.getenv("WEBAPP_URL", "https://google.com") 
-
-# Ваш Telegram ID для доступа к админке (узнайте у @userinfobot)
-ADMIN_ID = 123456789  
-
-# База данных
-
-DATABASE_NAME = "promo.db"
-
-# Для Render
-import os
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 WEBAPP_URL = os.getenv("WEBAPP_URL", "https://google.com")
 ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 DATABASE_URL = os.getenv("DATABASE_URL", "")
