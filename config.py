c41caafa9ba04e0d60e6e06107c9d5f219a5295d
Chevengur <i9954240136@gmail.com>
2026-02-22 21:53:24 +0300

    Delete config.py
    Remove config.py for security

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-# Читаем переменные окружения от Render
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-WEBAPP_URL = os.environ.get("WEBAPP_URL", "https://google.com")
-ADMIN_ID = int(os.environ.get("ADMIN_ID", "0"))
-DATABASE_URL = os.environ.get("DATABASE_URL", "")
